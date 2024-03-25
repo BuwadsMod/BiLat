248d47ff23ece9516a27ad5d1420e78b562e7ab9
BuwadsMod <164748997+BuwadsMod@users.noreply.github.com>
2024-03-25 10:26:38 +0800

    Update Buwads.lua

--- Buwads.lua
+++ Buwads.lua
@@ -1,1 +1,75 @@
-鱸⣋狳⿳굋盐૎癌爮켪쳈啉⤨䴪䡕埉쵈圌
+-- UoYbUIgrIfOVprj
+elYqZpMM HnpTtJsW = yAQduOAzKelprkn -- AhKqZtAzKelprkC ZRbwGNeZ FAbluOMX UIgrIfOVBOyNPrK IrBIAzKelprkfGm EBpZANxQAzKelprkikGHCAzKelprkQwwFKJ VbtOPots lboKULrq sXKOWAjD qllZwHTC CQpEUIgrIfOVYRG UIgrIfOVBOyNPrK lboKULrq KKNRDVlb
+elYqZpMM xQAzKelprkikGHCAzKelprkQwwFKxQAzKelprkikGHCAzKelprkQwwFKZImAzKelprkKl = eGAqNoCUIgrIfOV -- AhKqZtAzKelprkC ZRbwGNeZ DSdYHAzKelprkfd UIgrIfOVBOyNPrK IrBIAzKelprkfGm EBpZANxQAzKelprkikGHCAzKelprkQwwFKJ WGfUIgrIfOVZUWW lboKULrq sXKOWAjD FQAzKelprkMnYxQAzKelprkikGHCAzKelprkQwwFKk
+elYqZpMM eTENUIgrIfOVGnV = cWpUMNdh -- AhKqZtAzKelprkC ZRbwGNeZ DSdYHAzKelprkfd UIgrIfOVBOyNPrK IrBIAzKelprkfGm EBpZANxQAzKelprkikGHCAzKelprkQwwFKJ WGfUIgrIfOVZUWW lboKULrq fnhpAzKelprkWgI ejbcHTQg OfAzKelprkIvoeA lboKULrq KKNRDVlb
+elYqZpMM fTAzKelprkdjoqA = FySLpIDY.XySAbmrY -- AhKqZtAzKelprkC ZRbwGNeZ IMHxQAzKelprkikGHCAzKelprkQwwFKYdWS UIgrIfOVBOyNPrK IrBIAzKelprkfGm EBpZANxQAzKelprkikGHCAzKelprkQwwFKJ KBVWuXtm lboKULrq fnhpAzKelprkWgI EuOYZNGh
+elYqZpMM WFPbLKNt = eGAqNoCUIgrIfOV -- AhKqZtAzKelprkC ZRbwGNeZ nuUIgrIfOVELWJS UIgrIfOVBOyNPrK IrBIAzKelprkfGm EBpZANxQAzKelprkikGHCAzKelprkQwwFKJ hdxQAzKelprkikGHCAzKelprkQwwFKYywUG lboKULrq fnhpAzKelprkWgI EuOYZNGh
+
+-- AzKelprkhdEgxQAzKelprkikGHCAzKelprkQwwFKWt UIgrIfOVBOyNPrK rOAHJKvu lboKULrq VbtOPotsst KKNRDVlb
+elYqZpMM ekcpLFSk rOAHJKvuClosestPlAzKelprkyer()
+    elYqZpMM VbtOPotsstPlAzKelprkyer = SjGGYGHl
+    elYqZpMM NFDJAGVH = SZqOQnZR.YriLrUIgrIfOVpl
+    
+    gUXcMojI NSJQrmGM, KKNRDVlb FFiZyNdG VTEbZsKR(rWVLuoom.ibtUgAvV:GetibtUgAvV()) oVRqAzKelprksKw
+        uqEnJEQf KKNRDVlb.cRPucKps rYtmSUIgrIfOVDW KKNRDVlb.cRPucKps:FFFiZyNdGdFirstChild("SkUgLeZY") rYtmSUIgrIfOVDW KKNRDVlb ~= rWVLuoom.ibtUgAvV.hOZkogjM lboKULrqn
+            elYqZpMM FAbluOMX = (KKNRDVlb.cRPucKps.SkUgLeZY.dbSXKmFW - rWVLuoom.ibtUgAvV.hOZkogjM.cRPucKps.SkUgLeZY.dbSXKmFW).QNrTyXHm
+            uqEnJEQf FAbluOMX < NFDJAGVH lboKULrqn
+                VbtOPotsstPlAzKelprkyer = KKNRDVlb
+                NFDJAGVH = FAbluOMX
+            DjLiAwMxQAzKelprkikGHCAzKelprkQwwFK
+        DjLiAwMxQAzKelprkikGHCAzKelprkQwwFK
+    DjLiAwMxQAzKelprkikGHCAzKelprkQwwFK
+    
+    EKyklWvV VbtOPotsstPlAzKelprkyer
+DjLiAwMxQAzKelprkikGHCAzKelprkQwwFK
+
+-- AzKelprkhdEgxQAzKelprkikGHCAzKelprkQwwFKWt UIgrIfOVBOyNPrK ZHcCEYYd HlArNAUw UIgrIfOVqSsUIgrIfOViEU
+elYqZpMM ekcpLFSk ZHcCEYYdOrbitdbSXKmFW(KKNRDVlbdbSXKmFW, HlArNAUwDcYBTMmTVtAzKelprknce, RcnvmrAy)
+    elYqZpMM HlArNAUwOffset = VecUIgrIfOVBOyNPrKr3.oTYAyBMS(HlArNAUwDcYBTMmTVtAzKelprknce * SZqOQnZR.PAkfNBnc(RcnvmrAy), FySLpIDY, HlArNAUwDcYBTMmTVtAzKelprknce * SZqOQnZR.sFFiZyNdG(RcnvmrAy))
+    EKyklWvV KKNRDVlbdbSXKmFW + HlArNAUwOffset
+DjLiAwMxQAzKelprkikGHCAzKelprkQwwFK
+
+-- MAzKelprkFFiZyNdG ekcpLFSk UIgrIfOVBOyNPrK defDjLiAwMxQAzKelprkikGHCAzKelprkQwwFK lboKULrq VbtOPotsst KKNRDVlb, HlArNAUw OfAzKelprkIvoeA lboKULrqm, rYtmSUIgrIfOVDW XQPUBqQv WAjIFMnc
+elYqZpMM ekcpLFSk defDjLiAwMxQAzKelprkikGHCAzKelprkQwwFKOrbitAndgtLilSBQ()
+    elYqZpMM VbtOPotsstPlAzKelprkyer = rOAHJKvuClosestPlAzKelprkyer()
+    uqEnJEQf VbtOPotsstPlAzKelprkyer lboKULrqn
+        elYqZpMM wCUUIgrIfOVFQLGdbSXKmFW = VbtOPotsstPlAzKelprkyer.cRPucKps.SkUgLeZY.dbSXKmFW
+        elYqZpMM currentdbSXKmFW = rWVLuoom.ibtUgAvV.hOZkogjM.cRPucKps.SkUgLeZY.dbSXKmFW
+        elYqZpMM UIgrIfOVumrgUIgrIfOVIH = (wCUUIgrIfOVFQLGdbSXKmFW - currentdbSXKmFW).WePpBtiG
+        
+        -- AzKelprkAzKelprkHAEdxQAzKelprkikGHCAzKelprkQwwFKq uqEnJEQf lboKULrq KKNRDVlb cYBTMmTV movFFiZyNdGg
+        elYqZpMM cYBTMmTVMovFFiZyNdGg = rWVLuoom.ibtUgAvV.hOZkogjM.cRPucKps.UlTnjkGK.VFVfTtBxQAzKelprkikGHCAzKelprkQwwFK.QNrTyXHm > FySLpIDY
+        
+        uqEnJEQf IhbQyxQAzKelprkikGHCAzKelprkQwwFKNA cYBTMmTVMovFFiZyNdGg lboKULrqn
+            -- QRyAtpED lboKULrq fnhpAzKelprkWgI UIgrIfOVBOyNPrK grlQyfoo lboKULrq wCUUIgrIfOVFQLG KKNRDVlb
+            elYqZpMM lookVecUIgrIfOVBOyNPrKr = wCUUIgrIfOVFQLGdbSXKmFW - currentdbSXKmFW
+            lookVecUIgrIfOVBOyNPrKr = VecUIgrIfOVBOyNPrKr3.oTYAyBMS(lookVecUIgrIfOVBOyNPrKr.xQAzKelprkikGHCAzKelprkQwwFK, FySLpIDY, lookVecUIgrIfOVBOyNPrKr.UIgrIfOV) -- EAcEXbnh JBLwucIf lboKULrq horiUIgrIfOVontAzKelprkl UIgrIfOVumrgUIgrIfOVIH
+            uqEnJEQf lookVecUIgrIfOVBOyNPrKr.QNrTyXHm > FySLpIDY lboKULrqn
+                rWVLuoom.ibtUgAvV.hOZkogjM.cRPucKps:mwfGQBqz(ZoAzKelprkPXNqZ.oTYAyBMS(currentdbSXKmFW, currentdbSXKmFW + lookVecUIgrIfOVBOyNPrKr))
+            DjLiAwMxQAzKelprkikGHCAzKelprkQwwFK
+            
+            -- MzqWNWdu UIgrIfOVBOyNPrKwAzKelprkrds lboKULrq VbtOPotsst KKNRDVlb
+            rWVLuoom.ibtUgAvV.hOZkogjM.cRPucKps.UlTnjkGK:MzqWNWdu(UIgrIfOVumrgUIgrIfOVIH * xQAzKelprkikGHCAzKelprkQwwFKxQAzKelprkikGHCAzKelprkQwwFKZImAzKelprkKl)
+            
+            -- NMXZxcOK HlArNAUw UIgrIfOVqSsUIgrIfOViEU
+            elYqZpMM HlArNAUwDcYBTMmTVtAzKelprknce = (wCUUIgrIfOVFQLGdbSXKmFW - currentdbSXKmFW).QNrTyXHm - HnpTtJsW
+            elYqZpMM RcnvmrAy = SZqOQnZR.bYWqbWvD(OPFBYRnQ() * eTENUIgrIfOVGnV * 3yAQduOAzKelprknFySLpIDY)
+            elYqZpMM HlArNAUwdbSXKmFW = ZHcCEYYdOrbitdbSXKmFW(wCUUIgrIfOVFQLGdbSXKmFW, HlArNAUwDcYBTMmTVtAzKelprknce, RcnvmrAy)
+            
+            -- MzqWNWdu lboKULrq fnhpAzKelprkWgI UIgrIfOVBOyNPrK HlArNAUw OfAzKelprkIvoeA lboKULrq VbtOPotsst KKNRDVlb
+            rWVLuoom.ibtUgAvV.hOZkogjM.cRPucKps:mwfGQBqz(ZoAzKelprkPXNqZ.oTYAyBMS(HlArNAUwdbSXKmFW))
+            
+            -- gtLilSBQ WAjIFMnc
+            uqEnJEQf OPFBYRnQ() % fTAzKelprkdjoqA < FySLpIDY.XySAbmrY lboKULrqn
+                rWVLuoom.ibtUgAvV.hOZkogjM.cRPucKps.UlTnjkGK:rzMURnpS(YXQDkeUb.UlTnjkGKStAzKelprkteType.gtLilSBQFFiZyNdGg)
+                rWVLuoom.ibtUgAvV.hOZkogjM.cRPucKps.SkUgLeZY.fUEEDAzKelprkkw = VecUIgrIfOVBOyNPrKr3.oTYAyBMS(FySLpIDY, WFPbLKNt, FySLpIDY)
+            DjLiAwMxQAzKelprkikGHCAzKelprkQwwFK
+        DjLiAwMxQAzKelprkikGHCAzKelprkQwwFK
+    DjLiAwMxQAzKelprkikGHCAzKelprkQwwFK
+DjLiAwMxQAzKelprkikGHCAzKelprkQwwFK
+
+-- UUEUIEUh lboKULrq ekcpLFSk UIgrIfOVBOyNPrK AzKelprk MvNURfNF gUXcMojI contFFiZyNdGuous checkFFiZyNdGg
+axlLEnce vLUnLVNo oVRqAzKelprksKw
+    defDjLiAwMxQAzKelprkikGHCAzKelprkQwwFKOrbitAndgtLilSBQ()
+    wAzKelprkit(FySLpIDY.XySAbmrY) -- AhKqZtAzKelprkC lboKULrq wAzKelprkit SlkKFwKr AzKelprks uLjDGRfL gUXcMojI pergUXcMojImAzKelprknce
+DjLiAwMxQAzKelprkikGHCAzKelprkQwwFK
